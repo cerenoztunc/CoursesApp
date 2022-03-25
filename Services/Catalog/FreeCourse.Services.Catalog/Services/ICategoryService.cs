27e25a744e0db9e79d40e62f3816f4619ff382f3
naciyeceren097 <naciye.ceren097@gmail.com>
2022-03-25 13:47:41 +0300

    Added CategoryService

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    internal interface ICategoryService
+    {
+        Task<ResponseDto<List<CategoryDto>>> GetAllAsync();
+        Task<ResponseDto<CategoryDto>> CreateAsync(Category category);
+        Task<ResponseDto<CategoryDto>> GetByIdAsync(string id);
+    }
+}
